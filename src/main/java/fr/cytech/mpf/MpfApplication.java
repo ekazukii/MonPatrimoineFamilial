76f38ee376e083dc7643eadb46379a87cec9b956
Baptiste Loison <baptisteloison8400@gmail.com>
2023-10-03 10:39:54 +0200

    initial commit

---
+++ src/main/java/fr/cytech/mpf/MpfApplication.java
@@ -0,0 +1,13 @@
+package fr.cytech.mpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/cytech/mpf/MpfApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cytech.mpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
