fa5e3ad0a6d7ff8ffbdac71d501d66892534d152
Youbuze <tom.baillet2@gmail.com>
2023-10-04 13:34:29 +0200

    first repair

--- src/main/java/fr/cytech/mpf/controller/MailController.java
+++ src/main/java/fr/cytech/mpf/controller/MailController.java
@@ -1,18 +1,18 @@
-package fr.cytech.mpf.controller;
+    package fr.cytech.mpf.controller;
 
-import fr.cytech.mpf.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import fr.cytech.mpf.service.MailService;
+    import fr.cytech.mpf.service.UserService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import fr.cytech.mpf.service.MailService;
 
-@Controller
-public class MailController {
-    @Autowired
-    private MailService emailService;
+    @Controller
+    public class MailController {
+        @Autowired
+        private MailService emailService;
 
-    @PostMapping(value = "/sendmail")
-    public void sendMail() {
-            emailService.sendEmail("tom.baillet2@gmail.com", "monPapiFinito | Test mail back", "Bonjour Dark Baptiste, Vous voulez en finir avec votre Papi? Alors, vous êtes le bienvenue!");
+        @PostMapping(value = "/sendmail")
+        public void sendMail() {
+                emailService.sendEmail("tom.baillet2@gmail.com", "monPapiFinito | Test mail back", "Bonjour Dark Baptiste, Vous voulez en finir avec votre Papi? Alors, vous êtes le bienvenue!");
+        }
     }
-}
