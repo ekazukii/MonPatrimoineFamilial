1b5347e585e8d392bae9477ffd144fc7183e37cf
Youbuze <tom.baillet2@gmail.com>
2023-11-09 15:13:54 +0100

    Timeline chart souvenirs

--- src/main/java/fr/cytech/mpf/dto/CommAddDTO.java
+++ src/main/java/fr/cytech/mpf/dto/CommAddDTO.java
@@ -5,13 +5,11 @@ import lombok.Setter;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Date;
 
 @Getter
 @Setter
-public class MsgAddDTO implements Serializable {
+public class CommAddDTO implements Serializable {
     private long conv;
-    private long user_id;
+    private long souvenir;
     private String message;
-    private Date date;
 }
