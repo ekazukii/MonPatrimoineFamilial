2f86a44f1627aff676186bd0a387178b784635ee
anatolepain <anatole.pain@outlook.fr>
2023-12-27 02:43:51 +0100

    Add a new Tree page for TreeComponent

---
+++ src/main/frontend/src/pages/Tree.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import TreeComponent from '../components/TreeComponent';
+// import Container from "react-bootstrap/Container";
+// import styles from './souvenirs.module.css';
+// import {Image} from "react-bootstrap";
+// import Form from 'react-bootstrap/Form';
+// import InputGroupText from "react-bootstrap/InputGroupText"; // Importez les styles CSS gérés par les modules CSS
+// import SouvenirsComp from '../components/SouvenirsComponent.jsx'
+
+const Souvenirs = () => {
+    return (
+        <div>
+            <TreeComponent/>
+        </div>
+    );
+};
+
+export default Souvenirs;
